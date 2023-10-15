022f0c009eb95c8c5aafb80135f7bf14d3be7971
mariandev520 <manzomariano@hotmail.com>
2023-10-15 01:37:38 -0300

    rediseño inicio 1

--- app/src/main/java/com/xilonet/signa/view/Inicio.kt
+++ app/src/main/java/com/xilonet/signa/view/Inicio.kt
@@ -242,55 +242,6 @@ private fun FullHeader(navController: NavController,
 }
 
 
-@Composable
-private fun ButtonBelt(categoryNames: List<String>, currentCategory: String,
-                       changeCategory: (String) -> Unit){
-    LazyRow(){
-        items(categoryNames) {
-                category -> CategoryButton(category, category == currentCategory,
-            changeCategory)
-        }
-        item {
-            ButtonSpacer()
-        }
-    }
-}
-
-@Composable
-private fun CategoryButton(text: String,
-                           selected: Boolean = false,
-                           changeCategory: (String) -> Unit
-){
-    Row(){
-        ButtonSpacer()
-        Button(
-            onClick = {
-                changeCategory(text)
-                ClosePreviousVideo()
-                ClosePreviousVideo = {}
-                ScrollToTop()
-            },
-            colors = ButtonDefaults.buttonColors(
-                backgroundColor = if(selected) SignaDark else SignaLight
-            ),
-            modifier = Modifier
-                .width(108.dp)
-                .height(22.dp),
-            shape = RoundedCornerShape(100),
-            contentPadding = PaddingValues(0.dp)
-        ){
-            Text(text = text, style = MaterialTheme.typography.button,
-                color = if(selected) SignaLight else SignaDark)
-        }
-    }
-}
-
-@Composable
-private fun ButtonSpacer(){
-    Spacer(Modifier.width(8.dp))
-}
-
-
 
 @Composable
 private fun SearchBar(
@@ -333,30 +284,35 @@ private fun SearchBar(
                     modifier = Modifier.height(24.dp),
                     alpha = 0.5f
                 )
+
+                IconButton(
+                    onClick = {
+                        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
+                            putExtra(
+                                RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+                            )
+                            putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+                        }
+                        voiceRecognitionLauncher.launch(intent)
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Phone,
+                        contentDescription = "Reconocimiento de voz",
+                        tint = SignaDark
+
+                    )
+
+                }
             },
+
             modifier = Modifier
                 .weight(1f)
                 .padding(8.dp),
         )
 
-        IconButton(
-            onClick = {
-                val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
-                    putExtra(
-                        RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-                        RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
-                    )
-                    putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
-                }
-                voiceRecognitionLauncher.launch(intent)
-            }
-        ) {
-            Icon(
-                imageVector = Icons.Default.Phone,
-                contentDescription = "Reconocimiento de voz",
-                tint = SignaDark
-            )
-        }
+
     }
 }
 
@@ -499,203 +455,3 @@ private fun VideoPlayer(ctxt: Context, videoPath: String, exoPlayerManager: ExoP
 }
 
 
-@Composable
-fun InicioUIi(navController: NavController) {
-    val userInfo by remember { mutableStateOf(HTTPUserManager.getUserInfo()) }
-
-    val isPortrait = LocalConfiguration.current.orientation == Configuration.ORIENTATION_PORTRAIT
-
-    val scaleFactor =
-        if (isPortrait) 1f else 0.5f // Factor de escala del 50% en orientación horizontal
-
-    Image(
-        painter = painterResource(id = R.drawable.backa),
-        contentDescription = null,
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(1500.dp)
-            .alpha(0.4f)
-            .scale(scaleFactor), // Aplicar la escala al fondo
-        contentScale = ContentScale.Crop
-    )
-
-    Column {
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(240.dp * scaleFactor) // Aplicar la escala a la altura
-                .background(Color.Transparent)
-        ) {
-            Column(
-                modifier = Modifier
-                    .padding(16.dp)
-                    .fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Image(
-                    painter = painterResource(R.drawable.guest_user_profile_pic),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .size(80.dp * scaleFactor) // Aplicar la escala al tamaño de la imagen
-                        .clip(CircleShape)
-                        .background(Color.White)
-                )
-                Spacer(modifier = Modifier.height(8.dp * scaleFactor)) // Aplicar la escala al espacio
-                Text(
-                    text = userInfo?.firstName ?: "Invitado",
-                    style = MaterialTheme.typography.h5,
-                    color = Color.White
-                )
-            }
-        }
-
-
-        Spacer(modifier = Modifier.height(16.dp * scaleFactor)) // Aplicar la escala al espacio
-
-
-    }
-
-
-}
-
-
-
-
-@Composable
-private fun FullHeader(userInfo: UserInfo?){
-    Column(
-        verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
-            .requiredHeight(120.dp)
-            .fillMaxWidth()
-            .background(SignaGreen)
-    ) {
-        HeaderTitle(stringResource(R.string.inicio))
-        Spacer(Modifier.height(10.dp))
-        val fullUserName = if(userInfo != null) {
-            userInfo.firstName + " " + userInfo.lastName
-        } else {
-            stringResource(R.string.guest)
-        }
-        UserInfoBanner(nameToDisplay = fullUserName)
-        Log.d("LOGIN", fullUserName)
-    }
-}
-
-@Composable
-private fun UserInfoBanner(
-    nameToDisplay: String = stringResource(R.string.guest),
-    profilePic: Painter = painterResource(R.drawable.guest_user_profile_pic)
-){
-    Button(
-        onClick = {/* TODO: Se podría mostrar un drop-down con más información del usuario */},
-        colors = ButtonDefaults.buttonColors(SignaLight),
-        modifier = Modifier
-            .fillMaxWidth(0.9f)
-            .height(40.dp),
-        shape = RoundedCornerShape(40.dp),
-        contentPadding = PaddingValues(0.dp),
-    ) {
-        Image(
-            painter = profilePic,
-            contentDescription = null,
-            contentScale = ContentScale.Crop,
-            modifier = Modifier
-                .size(40.dp)
-                .clip(CircleShape)
-                .border(2.dp, SignaYellow, CircleShape),
-            alignment = Alignment.CenterStart
-        )
-        Spacer(Modifier.width(8.dp))
-        Text(
-            text = nameToDisplay,
-            style = MaterialTheme.typography.body2,
-            textAlign = TextAlign.Start,
-        )
-        Spacer(Modifier.weight(1f))
-
-    }
-}
-
-@Composable
-private fun InicioButton(
-    text: String,
-    graphicBgColor: Color,
-    icon: Painter,
-    onClick: () -> Unit
-) {
-    var isPressed by remember { mutableStateOf(false) }
-
-    val scale by animateFloatAsState(
-        targetValue = if (isPressed) 0.9f else 1f,
-        animationSpec = spring(stiffness = Spring.StiffnessLow)
-    )
-
-    Button(
-        onClick = {
-            onClick()
-            isPressed = true
-            // Restaurar el estado después de un breve retraso para la animación de pulsación.
-            GlobalScope.launch {
-                delay(100)
-                isPressed = false
-            }
-        },
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(200.dp)
-            .padding(16.dp)
-            .shadow(4.dp, RoundedCornerShape(8.dp), clip = false),
-        shape = RoundedCornerShape(8.dp),
-        border = BorderStroke(2.dp, SignaDark),
-        colors = ButtonDefaults.buttonColors(
-            backgroundColor = if (isPressed) SignaBackground else SignaGreen,
-            contentColor = Color.White
-        )
-    ) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(8.dp)
-                .clickable { onClick() }
-                .scale(scale) // Aplicar la animación de escala
-                .align(Alignment.CenterVertically)
-        ) {
-            BackgroundGraphicWithLogo(icon = icon, bgColor = graphicBgColor)
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(
-                text = text,
-                style = MaterialTheme.typography.h5,
-                textAlign = TextAlign.Center,
-                modifier = Modifier.fillMaxWidth()
-            )
-        }
-    }
-}
-
-
-
-@Composable
-private fun BackgroundGraphicWithLogo(size: Dp = 128.dp,
-                                      icon: Painter,
-                                      bgColor: Color
-){
-    Box(){
-        Image(
-            painterResource(R.drawable.background_pentagon),
-            null,
-            Modifier.size(size),
-            colorFilter = ColorFilter.tint(bgColor)
-        )
-        Image(
-            icon,
-            null,
-            Modifier
-                .zIndex(1f)
-                .size(size)
-                .padding(0.dp, 0.dp, 0.dp, 5.dp)
-        )
-    }
-}
